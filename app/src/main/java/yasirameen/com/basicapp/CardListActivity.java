8abcbb73122a0274675648d5b613f697ccdae2a9
Yasir Ameen <YasirAmeen92@gmail.com>
2017-06-18 12:31:08 +0500

    adasdf

---
+++ app/src/main/java/yasirameen/com/basicapp/CardListActivity.java
@@ -0,0 +1,15 @@
+package yasirameen.com.basicapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CardListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_list);
+
+
+    }
+}
